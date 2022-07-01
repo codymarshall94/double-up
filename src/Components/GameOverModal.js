75a121e6295f62fa9b672d16ca52bf209e55b47d
Cody Marshall <codymstuntsx@gmail.com>
2022-07-01 16:48:19 -0700

    removed close button, and leaderboard text

--- src/Components/GameOverModal.js
+++ src/Components/GameOverModal.js
@@ -1,38 +1,33 @@
-import React from 'react'
-import Modal from 'react-bootstrap/Modal'
-import Button from 'react-bootstrap/Button';
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
-function GameOverModal({level, showGameOverModal, handleClose, resetGame}) {
-  
-
+function GameOverModal({ level, showGameOverModal, handleClose, resetGame }) {
   return (
-    <div className='d-flex align-items-center'>
+    <div className="d-flex align-items-center">
       <Modal
-          show={showGameOverModal}
-          onHide={handleClose}
-          backdrop="static"
-          keyboard={false}
-          centered
-        >
-          <Modal.Header>
-            <Modal.Title>Modal title</Modal.Title>
-          </Modal.Header>
-          <Modal.Body className='modal-styles'>
-            <div>
-              <span>You Reached Level {level}</span>
-            </div>
-            <div className='d-flex justify-content-center'>
-              <span>LeaderBoard</span>
-            </div>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="primary" onClick={() => resetGame()}>Try Again</Button>
-          </Modal.Footer>
-        </Modal>
+        show={showGameOverModal}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+        centered
+      >
+        <Modal.Header>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="modal-styles">
+          <div className="d-flex justify-content-center">
+            <span>You Reached Level {level}</span>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={() => resetGame()}>
+            Try Again
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
 
-
 export default GameOverModal;
-
