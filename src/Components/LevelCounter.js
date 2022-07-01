cb086afa2717a67b521c515c9d52f9e9c3e223ef
Cody Marshall <codymstuntsx@gmail.com>
2022-07-01 14:50:48 -0700

    styles added

--- src/Components/LevelCounter.js
+++ src/Components/LevelCounter.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 function LevelCounter({level}) {
   return (
-    <div>Level: {level}</div>
+    <div className='level-container'>
+       <span className='mx-3'>Level: {level}</span>
+    </div>
+   
   )
 }
 
