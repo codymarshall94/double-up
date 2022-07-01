7f0ff7460430326450faa7871456108717c5331f
Cody Marshall <codymstuntsx@gmail.com>
2022-07-01 14:49:48 -0700

    removed userinput

--- src/Components/UserInput.js
+++ src/Components/UserInput.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+
 
 const min = 1;
 const max = 10;
 
-function UserInput({randomNumber, correctNumber, level, setLevel}) {
+function UserInput({randomNumber, correctNumber, level, setLevel, seconds, setSeconds}) {
     const [userGuess, setUserGuess] = useState("");
+    
 
     const isCorrect = (e) => {
         e.preventDefault();
@@ -14,18 +15,16 @@ function UserInput({randomNumber, correctNumber, level, setLevel}) {
 
         if(numberParsed === correctNumber) {
             randomNumber(min, max);
-            setLevel(level + 1)
-        } else {
-            
-        }
-        setUserGuess("")
+            setLevel(level + 1);
+            setSeconds(seconds + 1);
+          }
+            setUserGuess("")
     }
 
   return (
     <div>
         <Form onSubmit={isCorrect}>
-            <input type='text' value={userGuess} placeholder='Double the number shown!' onChange={(e) => setUserGuess(e.target.value)}/>
-            <Button type='submit'>Go</Button>
+            <input type='text' value={userGuess} className='user-input' placeholder='Double the number shown!' onChange={(e) => setUserGuess(e.target.value)}/>
         </Form>
     </div>
   )
