6c20ff02158e505750c163242ef746984c2225b4
Cody Marshall <codymstuntsx@gmail.com>
2022-07-07 13:13:18 -0700

    added sound effects for when answer correct/inc

--- src/Components/AnswerButtons.js
+++ src/Components/AnswerButtons.js
@@ -1,33 +1,57 @@
 import React, { useState } from "react";
-import '../css/answerbuttons.css';
+import "../css/answerbuttons.css";
+import useSound from "use-sound";
+import correctSfx from '../Sound/correct.mp3';
+import incorrectSfx from '../Sound/incorrect.mp3';
 
-function AnswerButtons({ answers, correctNumber, nextLevel, theme, setSeconds, seconds }) {
+function AnswerButtons({
+  answers,
+  correctNumber,
+  nextLevel,
+  setSeconds,
+  seconds,
+}) {
   const [selected, setSelected] = useState(null);
+  const [correct] = useSound(correctSfx);
+  const [incorrect] = useSound(incorrectSfx);
   let answerCorrect = true;
+  
 
-    //nextLevel gets called at Root if value is correct
+  //nextLevel gets called at Root if value is correct
   const isCorrect = (value) => {
-    setSelected(value)
-      if(value === correctNumber) {
-          nextLevel()
-          setSelected(null);
-        } else {
-          setSeconds(seconds - 1)
-          answerCorrect = false;
-          setTimeout(changeAnswerTrue, 1000);
-        }
+    setSelected(value);
+    if (value === correctNumber) {
+      correct();
+      nextLevel();
+      setSelected(null);
+    } else {
+      incorrect();
+      setSeconds(seconds - 1);
+      answerCorrect = false;
+      setTimeout(changeAnswerTrue, 1000);
     }
+  };
 
   const changeAnswerTrue = () => {
-      answerCorrect= true;
-      setSelected(null);
-    }
+    answerCorrect = true;
+    setSelected(null);
+  };
 
   if (answers) {
     return (
       <div className="answers-container">
         {answers.map((answer, index) => (
-          <button className={answerCorrect && selected !== answer ? "answer-button" : "answer-button answer-incorrect"} key={index} onClick={() => isCorrect(answer)}>{answer}</button>
+          <button
+            className={
+              answerCorrect && selected !== answer
+                ? "answer-button"
+                : "answer-button answer-incorrect"
+            }
+            key={index}
+            onClick={() => isCorrect(answer)}
+          >
+            {answer}
+          </button>
         ))}
       </div>
     );
