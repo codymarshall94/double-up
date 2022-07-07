bafd14468837e507e989f5f3db4527e4bea68852
Cody Marshall <codymstuntsx@gmail.com>
2022-07-07 12:36:30 -0700

    added a timeout to reset the wrong answers

--- src/Components/AnswerButtons.js
+++ src/Components/AnswerButtons.js
@@ -1,22 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import '../css/answerbuttons.css';
 
 function AnswerButtons({ answers, correctNumber, nextLevel, theme, setSeconds, seconds }) {
+  const [selected, setSelected] = useState(0);
+  let answerCorrect = true;
 
     //nextLevel gets called at Root if value is correct
-    const isCorrect = (value) => {
-        if(value === correctNumber) {
-            nextLevel()
-          } else {
-            setSeconds(seconds - 1)
-          }
+  const isCorrect = (value) => {
+    setSelected(value)
+      if(value === correctNumber) {
+          nextLevel()
+          setSelected(0);
+        } else {
+          setSeconds(seconds - 1)
+          answerCorrect = false;
+          setTimeout(changeAnswerTrue, 1000);
+        }
+    }
+
+  const changeAnswerTrue = () => {
+      answerCorrect= true;
+      setSelected(0);
     }
 
   if (answers) {
     return (
       <div className="answers-container">
         {answers.map((answer, index) => (
-          <button className={theme === 'light' ? "light-buttons answer-button" : "answer-button"} key={index} onClick={() => isCorrect(answer)}>{answer}</button>
+          <button className={answerCorrect && selected !== answer ? "answer-button" : "answer-button answer-incorrect" } key={index} onClick={() => isCorrect(answer)}>{answer}</button>
         ))}
       </div>
     );
